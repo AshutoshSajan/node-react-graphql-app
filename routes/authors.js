f2d3031ef662f2c7b0af422cb123e83dc6da3d70
sajan1231 <ashutoshsajan1213@gmail.com>
2020-03-21 20:31:46 +0530

    add post put patch delete for books route

--- routes/authors.js
+++ routes/authors.js
@@ -5,4 +5,24 @@ router.get(req, res, next, () => {
 	console.log("authors route");
 });
 
+router.post(req, res, next, () => {
+	console.log("authors post route");
+	res.send({ message: "hello world!" });
+});
+
+router.put(req, res, next, () => {
+	console.log("authors post route");
+	res.send({ message: "hello world!" });
+});
+
+router.patch(req, res, next, () => {
+	console.log("authors post route");
+	res.send({ message: "hello world!" });
+});
+
+router.delete(req, res, next, () => {
+	console.log("authors post route");
+	res.send({ message: "hello world!" });
+});
+
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get(req, res, next, () => {
-	console.log("books route");
+	console.log("books get route");
+});
+
+router.post(req, res, next, () => {
+	console.log("books get route");
+});
+
+router.put(req, res, next, () => {
+	console.log("books get route");
+});
+
+router.patch(req, res, next, () => {
+	console.log("books get route");
+});
+
+router.delete(req, res, next, () => {
+	console.log("books get route");
 });
 
 module.exports = router;
